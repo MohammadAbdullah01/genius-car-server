c80df3a727f8b7e9b2eb13d8fb1f4d807063c092
Mohammad-Abdullah <abdullahofficial2022@gmail.com>
2022-04-28 12:49:02 +0600

    performed changes

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ async function run() {
 
 run().catch(console.dir)
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello from genius car!')
 })
 
 app.listen(port, () => {
